79c1083c556f712559609c379970921af232c369
YedarmKim <oncoder@naver.com>
2017-11-29 00:32:00 +0900

    ㅁㅁㅁ

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,20 @@ module.exports = {
     title: '닌텐도 스위치 정발을 축하하며 12/1',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				property: 'og:title',
+				content: '닌텐도 스위치 12/1 정발 예정'
+			},
+			{
+				property: 'og:description',
+				content: '다음날 12/1 닌텐도 스위치가 정발이 됩니다. 행복합니다.'
+			},
+			{
+				property: 'og:image',
+				// content: 'http://www.seo-study.ga/63c1b5a5a3283393d8c950002ec6d826.png'
+				content: 'http://image.fmkorea.com/files/attach/new/20161229/486616/177782625/547240921/63c1b5a5a3283393d8c950002ec6d826.png'
+			}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
