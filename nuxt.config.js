f197789813fec3dbe3d80ecbcbfb269c4c999fdf
YedarmKim <oncoder@naver.com>
2017-11-29 07:08:33 +0900

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,12 +18,20 @@ module.exports = {
 			{
 				property: 'og:image',
 				// content: 'http://www.seo-study.ga/63c1b5a5a3283393d8c950002ec6d826.png'
-				content: '/switch.png'
+				content: 'http://www.seo-study.ga//switch.png'
+			},
+			{
+				property: 'og:image:width',
+				content: '600'
 			},
 			{
 				property: 'og:image',
 				// content: 'http://www.seo-study.ga/63c1b5a5a3283393d8c950002ec6d826.png'
-				content: '/mariodot.png'
+				content: 'http://www.seo-study.ga//mariodot.png'
+			},
+			{
+				property: 'og:image:width',
+				content: '500'
 			},
 			{
 				property: 'og:url',
