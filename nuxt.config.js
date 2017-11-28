fb2f76088f152c34ac9ff8ec0aa0787b86213690
YedarmKim <oncoder@naver.com>
2017-11-29 06:41:40 +0900

    aa

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
 			{
 				property: 'og:image',
 				// content: 'http://www.seo-study.ga/63c1b5a5a3283393d8c950002ec6d826.png'
-				content: '/mariodot.png'
+				content: '/swtich.png'
 			},
 			{
 				property: 'og:url',
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="container">
     <div>
-			<vue-mario
-				class="mario"
-				v-for="num of 5"
-				:key="num"></vue-mario>
+			<div class="mario-wrapper">
+				<vue-mario
+					class="ms"
+					v-for="theme of ['', 'mexico', 'amiibo', 'winter']"
+					:avatar="theme"
+					:key="theme"></vue-mario>
+			</div>
+			<img class="w" src="~assets/swtich.png" alt="">
       <h1 class="title">
         닌텐도 스위치 정발
       </h1>
@@ -45,19 +49,37 @@ export default {
   text-align: center;
 }
 
+@keyframes move {
+	0% {left: -600px;}
+	100% {left: 600px;}
+} 
+
+.mario-wrapper {
+	position: relative;
+	top: 30px;
+	overflow-x: hidden;
+	overflow-y: auto;
+}
+
+.ms {
+	margin: 0 10px;
+	position: relative;
+	animation: move 20s infinite linear;
+}
+
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
-  color: #35495e;
+  color: #48b0dd;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
   font-size: 42px;
-  color: #526488;
+  color: #48b0dd;
   word-spacing: 5px;
   padding-bottom: 15px;
 }
